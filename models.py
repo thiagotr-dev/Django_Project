f36e14a324178a4836e316271dbc2d1a16076e1e
Thiago Sales Terra <thiagosalestr@gmail.com>
2026-04-08 23:05:09 -0300

    Add files via upload

---
+++ models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
