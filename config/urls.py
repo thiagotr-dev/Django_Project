4ce35af630545b37bb3400957773d7bf5ed13f10
Thiago Sales Terra <thiagosalestr@gmail.com>
2026-04-08 23:13:11 -0300

    Add files via upload

---
+++ config/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from core import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
+    path('add/', views.add_product, name='add_product'),
+    path('remove/<int:id>/', views.remove_product, name='remove_product'),
+]
